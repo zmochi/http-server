51fe451df9a8de4b8a54c68ce002cafcd4bf4e44
zmochi <orian755@gmail.com>
2024-09-01 22:07:59 +0300

    queue.h: removed sentinel

--- src/queue.h
+++ src/queue.h
@@ -6,17 +6,14 @@
 #define __QUEUE_H
 
 struct queue {
-    struct list_item  sentinel;
     struct list_item *head;
 };
 
-static inline void init_queue(struct queue *queue) {
-    queue->head          = &queue->sentinel;
-    queue->sentinel.next = &queue->sentinel;
-    queue->sentinel.prev = &queue->sentinel;
-}
+static inline int is_empty(struct queue *queue) { return queue->head == NULL; }
 
 static inline struct list_item *dequeue(struct queue *queue) {
+    if ( is_empty(queue) ) return NULL;
+
     struct list_item *dequeued_entry = queue->head;
     queue->head                      = queue->head->prev;
     list_rm(dequeued_entry);
@@ -25,15 +22,14 @@ static inline struct list_item *dequeue(struct queue *queue) {
 }
 
 static inline void enqueue(struct queue *queue, struct list_item *new) {
-    list_add_head(new, queue->head);
+    if ( is_empty(queue) )
+        queue->head = new;
+    else
+        list_add_head(new, queue->head);
 }
 
 static inline struct list_item *peek(struct queue *queue) {
     return queue->head;
-}
-
-static inline int is_empty(struct queue *queue) {
-    return queue->head == &queue->sentinel;
 }
 
 #endif /* __QUEUE_H */

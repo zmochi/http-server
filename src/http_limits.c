71f1d94646f65905d5fa24c98688e3afdd1b9d26
zmochi <orian755@gmail.com>
2024-11-12 09:11:05 +0200

    Moved limits into http_limits.h only, using C23 constexpr

--- src/http_limits.c
+++
@@ -1,9 +0,0 @@
-const unsigned int BACKLOG = 64;
-const unsigned int INIT_RECV_BUFFER_SIZE = 256;
-const unsigned int INIT_SEND_BUFFER_CAPACITY = (1 << 10); // 1KB
-/* a buffer containing the path of request is allocated for each request */
-const unsigned int URI_PATH_LEN_LIMIT = 128;
-const unsigned int MAX_RECV_BUFFER_SIZE = (1 << 13); // 8KB
-const unsigned int MAX_SEND_BUFFER_SIZE = MAX_RECV_BUFFER_SIZE;
-const unsigned int SEND_REALLOC_MUL = 2;
-const unsigned int RECV_REALLOC_MUL = 2;
